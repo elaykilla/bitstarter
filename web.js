74d14863fd46b1161938ccac73a7c74c2278eeca
Elaykilla <abdoulaye.maiga@live.com>
2013-12-20 15:19:51 +0000

    added fs

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-   
-  response.send(fs.readFileSync("index.html").toString);
+    var index = fs.readFileSync('index.html').toString('utf-8');
+
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
